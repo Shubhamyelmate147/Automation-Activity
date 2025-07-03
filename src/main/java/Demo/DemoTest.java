2f50badc243c6ba5ebec5ba05dd926458fd11e58
Shubhamyelmate147 <shubham.yelmate@arihantcapital.com>
2025-07-03 14:54:14 +0530

    New logic add

--- src/main/java/Demo/DemoTest.java
+++ src/main/java/Demo/DemoTest.java
@@ -26,12 +26,27 @@ public class DemoTest {
       options.setDeviceName("OnePlus CPH2467");
       options.setPlatformName("Android");
       options.setAutomationName("UiAutomator2");
-      options.setNoReset(true);
       options.setUdid("bc20b511");
-      options.setApp("C:\\Users\\ARIHANT\\Desktop\\Appium\\ACML UAT 1.0.336.apk");
+      //options.setApp("C:\\Users\\ARIHANT\\Desktop\\Appium\\ACML UAT 1.0.336.apk");
+      options.setCapability("autoGrantPermissions", true);
+      options.setNoReset(true);
+
 
       driver = new AndroidDriver(new URL("http://192.168.80.142:4723"), options);
       System.out.println("App Launched Successfully");
+
+      String appPackage = "com.msf.acml.uat"; // your app package name
+
+      if (driver.isAppInstalled(appPackage)) {
+          System.out.println("✅ App already installed, launching it...");
+          driver.activateApp(appPackage);
+          enterPin("1111");
+      } else {
+          System.out.println("❌ App not installed, installing it...");
+          driver.installApp("C:\\Users\\ARIHANT\\Desktop\\Appium\\ACML UAT 1.0.336.apk");
+          driver.activateApp(appPackage);
+      }
+
 
       WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2000));
       WebElement loginOption = wait.until(
@@ -59,15 +74,16 @@ public class DemoTest {
               ExpectedConditions.presenceOfElementLocated(AppiumBy.accessibilityId("Proceed")));
       loginButton.click();
 
-      //===========
-      Thread.sleep(1000);
-      WebElement pinField = wait.until(
-              ExpectedConditions.presenceOfElementLocated(By.className("android.widget.EditText")));
-      pinField.click();
-      pinField.sendKeys("1111");
+      //=========
+//      Thread.sleep(1000);
+//      WebElement pinField = wait.until(
+//              ExpectedConditions.presenceOfElementLocated(By.className("android.widget.EditText")));
+//      pinField.click();
+//      pinField.sendKeys("1111");
+
+      enterPin("1111");
 
       System.out.println("✅ Login and PIN entry successful.");
-
 
 
       WebElement  understand = wait.until(
@@ -84,8 +100,55 @@ public class DemoTest {
             ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.ImageView[@content-desc=\"Bank Accounts\n" +
                     "Bank details\"]")));
     BankAccounts.click();
+    Thread.sleep(1000);
+    driver.findElement(By.xpath("//android.view.View[@content-desc='Bank Accounts']/android.widget.ImageView")).click();
 
+    Thread.sleep(2000);
+    WebElement  IPO = wait.until(
+            ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.ImageView[@content-desc=\"IPOs/NCDs\n" +
+                    "Invest online in IPOs/NCDs\"]")));
+    IPO.click();
+    Thread.sleep(1000);
+    driver.findElement(By.xpath("//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View[1]/android.view.View/android.widget.ImageView")).click();
+
+
+    Thread.sleep(2000);
+    WebElement ResearchCalls = wait.until(
+            ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.ImageView[@content-desc=\"Research Calls\n" +
+                    "Research stock ideas from experts\"]")));
+    ResearchCalls.click();
+    Thread.sleep(4000);
+      driver.findElement(By.xpath("//android.view.View[@content-desc=\"Continue\"]")).click();
+      driver.findElement(By.xpath("//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View[1]/android.view.View/android.widget.ImageView")).click();
+
+    Thread.sleep(2000);
+      WebElement SmartReports = wait.until(
+              ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.ImageView[@content-desc=\"Smart Reports\n" + "Your trading reports\"]")));
+      SmartReports.click();
+
+      Thread.sleep(2000);
+      WebElement MyAlerts = wait.until(
+              ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.ImageView[@content-desc=\"My Alerts\n" + "Manage Alerts\"]")));
+      MyAlerts.click();
+      Thread.sleep(1000);
+      driver.findElement(By.xpath("//android.view.View[@content-desc=\"My Alerts\"]/android.view.View[1]/android.widget.ImageView")).click();
+
+      Thread.sleep(2000);
+      WebElement Notification = wait.until(
+              ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View[1]/android.view.View/android.view.View/android.view.View/android.widget.ImageView[3]")));
+      Notification.click();
+      Thread.sleep(1000);
+      driver.findElement(By.xpath("//android.widget.ImageView[@content-desc=\"Notifications\"]")).click();
   }
+
+    public void enterPin(String pin) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        WebElement pinField = wait.until(
+                ExpectedConditions.presenceOfElementLocated(By.className("android.widget.EditText")));
+        pinField.click();
+        pinField.sendKeys(pin);
+        System.out.println("✅ PIN entered successfully.");
+    }
 }
 
 
